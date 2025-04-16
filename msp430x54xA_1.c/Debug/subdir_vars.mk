983bff90fb9a1aa470eb7e1f9cdf25f96e207059
Abdelaziz Salah <abdelaziz132001@gmail.com>
2025-04-16 23:52:10 +0200

    finishing first Lab1

---
+++ msp430x54xA_1.c/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5438a.cmd 
+
+C_SRCS += \
+../msp430x54xA_1.c 
+
+C_DEPS += \
+./msp430x54xA_1.d 
+
+OBJS += \
+./msp430x54xA_1.obj 
+
+OBJS__QUOTED += \
+"msp430x54xA_1.obj" 
+
+C_DEPS__QUOTED += \
+"msp430x54xA_1.d" 
+
+C_SRCS__QUOTED += \
+"../msp430x54xA_1.c" 
+
+
---
+++ noteOnCom.md
@@ -0,0 +1,3 @@
+* open the Device manager, and check on which port is the device connected.
+
+* always remember to build the project
